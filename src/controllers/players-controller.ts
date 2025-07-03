598b1d14489b7a321d98b6b1793495113c644ddf
gustavogss <gustavogss.dev@gmail.com>
2025-07-03 14:33:59 -0300

    feat: try-catch controller

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,41 +1,60 @@
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
 import { 
   getPlayerByIdService, 
   getPlayerService, 
   postPlayerService,
   deletePlayerService,
   updatePlayerService,
-} from '../services/players-service'
-import { StaticsModel } from '../models/statics-model'
+} from '../services/players-service';
+import { StaticsModel } from '../models/statics-model';
+import statuscode from 'http-status-codes';
 
 export const getPlayerController = async (req: Request, res: Response) => {
-  const httpResponse = await getPlayerService()
-  res.status(httpResponse.status).json(httpResponse.data)
-}
+  try {
+    const httpResponse = await getPlayerService();
+    res.status(httpResponse.status).json(httpResponse.data);
+  } catch (error) {
+    res.status(statuscode.INTERNAL_SERVER_ERROR).json({ message: "Erro interno ao buscar jogadores." });
+  }
+};
 
 export const getPlayerByIdController = async (req: Request, res: Response) => {
- const id = parseInt(req.params.id)
- const httpResponse = await getPlayerByIdService(id)
- res.status(httpResponse.status).json(httpResponse.data)
-}
+  try {
+    const id = parseInt(req.params.id);
+    const httpResponse = await getPlayerByIdService(id);
+    res.status(httpResponse.status).json(httpResponse.data);
+  } catch (error) {
+    res.status(statuscode.INTERNAL_SERVER_ERROR).json({ message: "Erro interno ao buscar jogador por ID." });
+  }
+};
 
 export const postPlayerController = async (req: Request, res: Response) => {
-  const bodyValue = req.body
-  const httpResponse = await postPlayerService(bodyValue);
-  if(httpResponse){
-    res.status(httpResponse.status).json(httpResponse.data)
+  try {
+    const bodyValue = req.body;
+    const httpResponse = await postPlayerService(bodyValue);
+    res.status(httpResponse.status).json(httpResponse.data);
+  } catch (error) {
+    res.status(statuscode.INTERNAL_SERVER_ERROR).json({ message: "Erro interno ao cadastrar jogador." });
   }
-}
+};
 
 export const deletePlayerController = async (req: Request, res: Response) => {
-  const id = parseInt(req.params.id)
-  const httpResponse = await deletePlayerService(id);
-  res.status(httpResponse.status).json(httpResponse.data)
-}
+  try {
+    const id = parseInt(req.params.id);
+    const httpResponse = await deletePlayerService(id);
+    res.status(httpResponse.status).json(httpResponse.data);
+  } catch (error) {
+    res.status(statuscode.INTERNAL_SERVER_ERROR).json({ message: "Erro interno ao deletar jogador." });
+  }
+};
 
-export const updatePlayerController = async (req: Request, res:Response) => {
-  const id = parseInt(req.params.id)
-  const bodyValue: StaticsModel = req.body
-  const httpResponse = await updatePlayerService(id, bodyValue);
-  res.status(httpResponse.status).json(httpResponse.data)
-}
+export const updatePlayerController = async (req: Request, res: Response) => {
+  try {
+    const id = parseInt(req.params.id);
+    const bodyValue: StaticsModel = req.body;
+    const httpResponse = await updatePlayerService(id, bodyValue);
+    res.status(httpResponse.status).json(httpResponse.data);
+  } catch (error) {
+    res.status(statuscode.INTERNAL_SERVER_ERROR).json({ message: "Erro interno ao atualizar estatísticas do jogador." });
+  }
+};
